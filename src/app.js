3571c415f91b7159735823c5a513494134c156b5
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-01-29 07:34:01 +0330

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,20 @@
+ function warn(){
+     for (let i = 0; i < arguments.length; i++){
+      console.warn(arguments[i]);
+   }
+ }
+ function log(){
+    for (let i = 0; i < arguments.length; i++){
+      console.log(arguments[i]);
+   }
+ }
+ 
+ function error(){
+   for (let i = 0; i < arguments.length; i++){
+      console.error(arguments[i]);
+   }
+ }
+ function clear(){
+ 	// console.clear() method
+	console.clear();
+ }
